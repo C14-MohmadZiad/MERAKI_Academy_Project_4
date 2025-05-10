c88fbe70e655aa9092652826b0f2ce3e54736d11
C14-MohmadZiad <mohmmadziad.ali@gmail.com>
2025-05-11 01:59:08 +0300

    Add ProductDetail + carSLice

--- frontend/src/pages/ProductDetail/index.jsx
+++ frontend/src/pages/ProductDetail/index.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/cartSlice";
+import "./style.css";
 
+const ProductDetail = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const [product, setProduct] = useState(null);
 
-const ProductDetail = ()=> {
+  useEffect(() => {
+    api
+      .get(`products/${id}`)
+      .then((res) => {
+        setProduct(res.data);
+      })
+      .catch((err) => {
+        console.error("Error fetching product ", err);
+      });
+  }, [id]);
+  const handleAddToCart = () => {
+    dispatch(addToCart(product));
+    alert("Product added to cart!");
+  };
+  if (!product) {
+    return <p>Loading product...</p>;
+  }
 
-return <h2> Product details page</h2>
+  return (
+    <div className="product-detail">
+      <img src={product.image} alt={product.name} />
+      <h2>{product.name}</h2>
+      <p>{product.description}</p>
+      <h3>{product.price} JOD</h3>
+      <button onClick={handleAddToCart}>Add to Cart</button>
+    </div>
+  );
+};
 
-
-}
-
-export default ProductDetail
+export default ProductDetail;
---
+++ frontend/src/redux/cartSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      const existing = state.items.find(
+        (item) => item._id === action.payload._id
+      );
+      if (existing) {
+        existing.quantity += 1;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeFromCart: (state, action) => {
+      state.items = state.items.filter((item) => item._id !== action.payload);
+    },
+    incrementQty: (state, action) => {
+      const item = state.items.find((item) => item._id === action.payload);
+      if (item) item.quantity += 1;
+    },
+    decrementQty: (state, action) => {
+      const item = state.items.find((item) => item._id === action.payload);
+      if (item && item.quantity > 1) {
+        item.quantity -= 1;
+      } else {
+        // delete if the product = 0
+        state.items = state.items.filter((i) => i._id !== action.payload);
+      }
+    },
+    clearCart: (state) => {
+      state.items = [];
+    },
+  },
+});
+
+export const {
+  addToCart,
+  removeFromCart,
+  incrementQty,
+  decrementQty,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
