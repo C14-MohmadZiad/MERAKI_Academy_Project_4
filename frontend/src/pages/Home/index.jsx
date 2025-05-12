35d34957011ff24370cdae26c5fdfdc0ca35c4cc
C14-MohmadZiad <mohmmadziad.ali@gmail.com>
2025-05-12 20:12:26 +0300

    Css Home Page

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -15,7 +15,6 @@ const Home = () => {
     api
       .get("/products")
       .then((res) => {
-
         dispatch(setProducts(res.data));
       })
       .catch((err) => {
@@ -24,10 +23,27 @@ const Home = () => {
   }, [dispatch]);
 
   return (
-    <div className="home-grid">
-      {items.map((prod) => (
-        <ProductCard key={prod._id} product={prod} />
-      ))}
+    <div className="home-container">
+      <section className="hero">
+        <div className="hero-content">
+          <h1>Subscribe to the Best Internet Offers</h1>
+          <p>
+            High speed , affordable price, and exclusive entertainment bundles
+          </p>
+          <button className="hero-btn">Explore Plans</button>
+        </div>
+        <div className="hero-image">
+          <img src="https://media.istockphoto.com/id/2170143478/photo/happy-woman-using-smartphone-and-credit-card-for-online-shopping-on-sofa-at-home.jpg?s=612x612&w=0&k=20&c=nsxM2tpJSwjpnuTF1djtQbUgQlTzRgsJaEKKw2_4U1s=" />
+        </div>
+      </section>
+      <section className="products-section">
+        <h2 className="section-title">Our Top Fiber Plans </h2>
+        <div className="product-grid">
+          {items.map((prod) => (
+            <ProductCard key={prod._id} product={prod} />
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
