becc8d521a581044dfe917e165604283dbab6a1c
C14-MohmadZiad <mohmmadziad.ali@gmail.com>
2025-05-12 21:12:00 +0300

    Design NabBar

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,45 +1,73 @@
 import React, { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import "./style.css";
 
 const Navbar = () => {
   const { isLoggedIn, logoutUser, user } = useContext(AuthContext);
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleLogout = () => {
     logoutUser();
     navigate("/login");
   };
 
+  const handleSearch = (e) => {
+    if (e.key === "Enter") {
+      navigate(`/?search=${searchTerm}`);
+    }
+  };
+
   return (
     <nav className="navbar">
-      <div className="navbar__brand">
-        <Link to="/">MZ-Store</Link>
+      <div className="navbar__left">
+        <Link to="/" className="navbar__logo">
+          MZ-Store
+        </Link>
+      </div>
+      <div className="navbar__search-container">
+        <input
+          type="text"
+          placeholder="Search for ..."
+          className="navbar__search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={handleSearch}
+        />
+        <button
+          className="navbar__search-icon"
+          onClick={() => navigate(`/?search=${searchTerm}`)}
+        >
+          <img src="https://media.istockphoto.com/id/2065067084/vector/3d-orange-magnifying-glass-icon-isolated-on-gray-background-render-minimal-transparent-loupe.jpg?s=612x612&w=0&k=20&c=lB7ASdrCl0uTWdXVlFILfuiLtL2EH6zYDqXpnT48n84=" alt="search" />
+        </button>
       </div>
 
-      <button
-        className="navbar__toggle"
-        onClick={() => setMenuOpen((open) => !open)}
-      >
-        ☰
-      </button>
+      <div className="navbar__right">
+        <Link to="/" className="navbar__link">
+          Home
+        </Link>
+        <Link to="/cart" className="navbar__link">
+          Cart
+        </Link>
 
-      <div className={`navbar__links ${menuOpen ? "open" : ""}`}>
-        <Link to="/">Home</Link>
-        <Link to="/cart">Cart</Link>
-
-        {user?.role=== "provider" && (
-          <Link to="/add-product">Add Product</Link>
+        {user?.role === "provider" && (
+          <Link to="/add-product" className="navbar__link">
+            Add Product
+          </Link>
         )}
 
         {user?.role === "user" && (
-          <Link to="/request-provider">Become a Provider</Link>
+          <Link to="/request-provider" className="navbar__link">
+            Become a Provider
+          </Link>
         )}
 
         {user?.role === "admin" && (
-          <Link to="/admin/requests">Provider Requests</Link>
+          <Link to="/admin/requests" className="navbar__link">
+            Requests
+          </Link>
         )}
 
         {isLoggedIn ? (
@@ -48,8 +76,12 @@ const Navbar = () => {
           </button>
         ) : (
           <>
-            <Link to="/login">Sign In</Link>
-            <Link to="/register">Create Account</Link>
+            <Link to="/login" className="navbar__link">
+              Sign In
+            </Link>
+            <Link to="/register" className="navbar__link">
+              Create Account
+            </Link>
           </>
         )}
       </div>
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import api from "../../services/api";
 import { setProducts } from "../../redux/productSlice";
 import ProductCard from "../../components/ProductCard";
+import { useSearchParams } from "react-router-dom";
 
 import "./style.css";
 
 const Home = () => {
   const dispatch = useDispatch();
-
   const items = useSelector((state) => state.products.items);
-
+  const [searchParams] = useSearchParams();
+  const searchTerm = searchParams.get("search") || "";
+  
   useEffect(() => {
     api
       .get("/products")
@@ -22,24 +24,31 @@ const Home = () => {
       });
   }, [dispatch]);
 
+  const filteredItems = items.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="home-container">
       <section className="hero">
         <div className="hero-content">
           <h1>Subscribe to the Best Internet Offers</h1>
           <p>
-            High speed , affordable price, and exclusive entertainment bundles
+            High speed, affordable price, and exclusive entertainment bundles
           </p>
+
           <button className="hero-btn">Explore Plans</button>
         </div>
+
         <div className="hero-image">
           <img src="https://media.istockphoto.com/id/2170143478/photo/happy-woman-using-smartphone-and-credit-card-for-online-shopping-on-sofa-at-home.jpg?s=612x612&w=0&k=20&c=nsxM2tpJSwjpnuTF1djtQbUgQlTzRgsJaEKKw2_4U1s=" />
         </div>
       </section>
+
       <section className="products-section">
-        <h2 className="section-title">Our Top Fiber Plans </h2>
+        <h2 className="section-title">Our Top Fiber Plans</h2>
         <div className="product-grid">
-          {items.map((prod) => (
+          {filteredItems.map((prod) => (
             <ProductCard key={prod._id} product={prod} />
           ))}
         </div>
